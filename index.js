cbb752048510eda61c691c9fb71c4039365f87e9
masud rana <masudfcs1@gmail.com>
2023-12-02 07:06:39 +0600

    Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-require("./models");
+const User = require("./models/user");
 var colors = require("@colors/colors");
 
 app.use(bodyParser.json());
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+
+User.sync({ force: true });
 
 app.listen(3000, () => {
   console.log("App will run on: http://localhost:3000".bgMagenta);
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("./index");
+
+const User = sequelize.define(
+  "User",
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    }
+  },
+  {
+    // Other model options go here
+    tableName: "users"
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
+module.exports = User;
